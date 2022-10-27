1b2d9cf3fcebfd8ab6fba850ba346fce05b8f45e
Filipe André <110608654+Flipedds@users.noreply.github.com>
2022-10-27 12:31:48 +0000

    Initial commit

---
+++ turtlecont.py
@@ -0,0 +1,38 @@
+import turtle
+turtle.bgcolor("black")
+turtle.pensize(2)
+turtle.speed(10)
+turtle.shape("turtle")
+contador = 1
+while contador = <2:
+	for i in range(12):
+	for colors in["red","magenta","blue","orange","white"]:
+		turtle.color(colors)
+		turtle.right(10)
+		turtle.fd(10)
+		turtle.left(4)
+		turtle.stamp()
+	for i in range(12):
+	for colors in ["red","magenta","blue","orange","white"]:
+		turtle.color(colors)
+		turtle.left(10)
+		turtle.fd(10)
+		turtle.right(4)
+		turtle.stamp()
+	for i in range(12):
+	for colors in ["red","magenta","blue","orange","white"]:
+		turtle.color(colors)
+		turtle.right(10)
+		turtle.fd(15)
+		turtle.left(4)
+		turtle.stamp()
+		
+	for i in range(12):
+	for colors in ["red","magenta","blue","orange","white"]:
+		turtle.color(colors)
+		turtle.left(10)
+		turtle.fd(12)
+		turtle.right(4)
+		turtle.stamp()
+		
+turtle.hideturtle()
